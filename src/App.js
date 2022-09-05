d093c93cc9be43683770cf7879cd157fd7b88b49
Arif5577 <arifalamm33@gmail.com>
2022-09-05 16:20:46 +0600

    home page design done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Hire from "./components/Hire";
+import Icons from "./components/Icons";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route index element={<Icons />} />
+          <Route path="about" element={<About />} />
+          <Route path="hire" element={<Hire />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,51 @@
-import React, { useState } from "react";
-import { FaTimes } from "react-icons/fa";
-const Home = () => {
-  const [showSideBar, setShowSideBar] = useState(true);
-  return (
-    <section className="home relative">
-      <div
-        className={`bg-slate-900 min-h-screen w-2/5 flex flex-col items-center justify-center absolute left-0 top-0  ${
-          showSideBar ? "translate-x-0" : "-translate-x-full"
-        } duration-300`}
-      >
-        <div className="container  ">
-          <button
-            className="absolute text-gray-900 h-8 w-8 bg-gray-50 text-center flex items-center justify-center right-0 top-0 border-2 border-slate-900"
-            onClick={() => setShowSideBar(!showSideBar)}
-          >
-            <FaTimes />
-          </button>
-          <p className="text-gray-50">Hi...</p>
-          <h1 className="text-gray-50 text-3xl my-3 font-bold">
-            I AM <span className="text-orange-500">MD ARIFUL ISLAM</span>
-          </h1>
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
-          <p className="text-gray-50 text-sm">
-            A passionate Junior Javascript developer from Bangladesh. I'm highly
-            passionate about JavaScript-based software development technologies
-            such as: MERN STACK
-          </p>
-          <div className="mt-5">
-            <div className="flex items-center gap-3">
-              <button className="btn">about me</button>
-              <button className="btn">hire me</button>
-            </div>
-            <div className="flex items-center gap-3 mt-3">
-              <button className="btn">dawnload cv</button>
-              <button className="btn">my skills</button>
-            </div>
-            <div className="flex items-center gap-3 mt-3">
-              <button className="btn">projects</button>
+const Home = () => {
+  return (
+    <section className="py-0">
+      <div className="grid grid-cols-5">
+        <div className="bg-gradient-to-l from-slate-800 to to-black min-h-screen col-span-2  flex flex-col items-center justify-center">
+          <div className="container">
+            <p className="text-gray-50">Hi...</p>
+            <h1 className="text-gray-50 text-3xl my-3 font-bold">
+              I AM <span className="text-orange-500">MD ARIFUL ISLAM</span>
+            </h1>
+
+            <p className="text-gray-50 text-sm">
+              A passionate Junior Javascript developer from Bangladesh. I'm
+              highly passionate about JavaScript-based software development
+              technologies such as: MERN STACK
+            </p>
+            <div className="mt-5">
+              <div className="flex items-center gap-3">
+                <Link to="/about" className="btn">
+                  about me
+                </Link>
+                <Link to="/hire" className="btn">
+                  hire me
+                </Link>
+              </div>
+              <div className="flex items-center gap-3 mt-3">
+                <button className="btn">dawnload cv</button>
+                <Link to="/skills" className="btn">
+                  my skills
+                </Link>
+              </div>
+              <div className="flex items-center gap-3 mt-3">
+                <Link to="/projects" className="btn">
+                  projects
+                </Link>
+                <Link to="/" className="btn">
+                  home
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className=" bg-gray-100 ">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto
-          laboriosam, asperiores ad quae dolor est voluptatem in! Enim hic
-          recusandae provident natus atque totam molestiae mollitia? Distinctio
-          nihil cum laboriosam. Porro tempora et modi neque ea quia ullam
-          voluptatum mollitia dolore quidem vitae corrupti reprehenderit eius
-          nemo ratione, minus temporibus consectetur velit odit. Et illo ex
-          provident modi obcaecati possimus, quae omnis? Asperiores, atque! Cum
-          iste aliquid praesentium quis inventore ab aspernatur earum molestiae
-          fuga similique minus eveniet pariatur ullam error fugiat rerum tenetur
-          provident et dicta culpa voluptas explicabo, deleniti non?
-          Exercitationem cupiditate dicta, vero officia inventore soluta!
-        </p>
+        <div className="col-span-3 min-h-screen flex flex-col gap-5 items-center justify-center">
+          <Outlet />
+        </div>
       </div>
     </section>
   );
